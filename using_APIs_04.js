5ce5a969964fd9554ae87c9b1172b0c96c4bce28
Cormac Donohoe <57358706+cormac0108@users.noreply.github.com>
2020-08-14 00:06:46 +0100

    API use

---
+++ using_APIs_04.js
@@ -0,0 +1,11 @@
+
+//star wars API 
+fetch('https://swapi.py4e.com/api/people/4/')
+    .then(resp => resp.json())
+    .then(console.log)
+
+ fetch('https://swapi.py4e.com/api/people/6/')
+    .then(resp => resp.json())
+    .then(console.log)
+
+// IBM Watson API 
